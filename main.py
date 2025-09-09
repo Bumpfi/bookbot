191414c6ea6c3f17cc79af180222bfef3336bc47
Bumpfi <felix.kargl@live.com>
2025-09-09 11:57:43 +0200

    updated main function - prints book

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(filepath):
+    file_contents = None
+
+    with open(filepath) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+
+main()
