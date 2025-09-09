6f0b6033bf713ece1381533ed44f55d7bfc36673
Bumpfi <felix.kargl@live.com>
2025-09-09 12:07:04 +0200

    updating main - book word counter

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ def get_book_text(filepath):
     return file_contents
 
 
+def count_words(filepath):
+    text = get_book_text(filepath)
+    words = text.split()
+    words_count = 0
+
+    for word in words:
+        words_count += 1
+
+    return words_count
+
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    words_count = count_words("./books/frankenstein.txt")
+    print(f"{words_count} words found in the document")
 
 
 main()
